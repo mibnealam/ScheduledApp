bf4f18556fc986fc663ff1dcba49ea1ee849a761
Mahfuj <mibnealam@gmail.com>
2018-09-10 16:32:45 +0600

    Add sign up page design

--- app/src/main/java/com/example/mibne/scheduledapp/LoginActivity.java
+++ app/src/main/java/com/example/mibne/scheduledapp/LoginActivity.java
@@ -1,13 +1,42 @@
 package com.example.mibne.scheduledapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.sign_up_button).setOnClickListener((View.OnClickListener) this);
+    }
+
+    // [START signUp]
+    private void signUp() {
+        Intent signUpInIntent = new Intent(this, SignUpActivity.class);
+        startActivity(signUpInIntent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.log_in_button:
+                //logIn();
+                break;
+            case R.id.sign_up_button:
+                signUp();
+                break;
+            case R.id.log_in_with_google_button:
+                //signIn();
+                break;
+            //case R.id.disconnect_button:
+            //revokeAccess();
+            //break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/mibne/scheduledapp/SignUpActivity.java
@@ -0,0 +1,34 @@
+package com.example.mibne.scheduledapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+        Spinner orgSpinner = (Spinner) findViewById(R.id.organizations_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> orgSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.organizations_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        orgSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        orgSpinner.setAdapter(orgSpinnerAdapter);
+
+        Spinner deptSpinner = (Spinner) findViewById(R.id.departments_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> deptSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.departments_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        deptSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        deptSpinner.setAdapter(deptSpinnerAdapter);
+    }
+}
